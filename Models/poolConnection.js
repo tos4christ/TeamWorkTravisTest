bc31f6a737a124be0fd5df16eb07b518fc94b245
Tos4christ <glmmission@hotmail.com>
2019-11-07 22:49:09 +0100

    update queries callback for database

--- Models/poolConnection.js
+++ Models/poolConnection.js
@@ -4,7 +4,7 @@ const query = require('./poolQuery');
 const pool = new Pool({
   user: process.env.PGUSER,
   host: process.env.PGHOST,
-  databse: process.env.PGDATABASE,
+  database: process.env.PGDATABASE,
   port: process.env.PGPORT,
   password: process.env.PGPASSWORD,
   connectionTimeoutMillis: 0,
