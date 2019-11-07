cff61a4d6aedc506bcbb1f275d2086290b0c6795
Tos4christ <glmmission@hotmail.com>
2019-11-07 12:25:50 +0100

    ft-upload-files-to-test

--- Models/poolConnection.js
+++ Models/poolConnection.js
@@ -1,8 +1,17 @@
 const { Pool } = require('pg');
 
-pool = new Pool();
+const pool = new Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  databse: process.env.PGDATABASE,
+  port: process.env.PGPORT,
+  password: process.env.PGPASSWORD,
+  connectionTimeoutMillis: 0,
+  idleTimeoutMillis: 600000,
+  max: 50
+})
 
-pool.query(`CREATE TABLE employees(employee_id integer NOT NULL DEFAULT, firstname text NOT NULL,
+pool.query(`CREATE TABLE employees(employee_id integer NOT NULL, firstname text NOT NULL,
     lastname text NOT NULL,
     email text NOT NULL,
     employee_password text NOT NULL,
@@ -13,7 +22,7 @@ pool.query(`CREATE TABLE employees(employee_id integer NOT NULL DEFAULT, firstna
     creation_date date NOT NULL,
     CONSTRAINT employees_pkey PRIMARY KEY (employee_id),
     CONSTRAINT employees_email_key UNIQUE (email),
-    CONSTRAINT employees_employee_no_key UNIQUE (employee_no)) ALTER TABLE public.employees OWNER TO postgres`)
+    CONSTRAINT employees_employee_no_key UNIQUE (employee_no)) ALTER TABLE employees OWNER TO postgres`)
   .then( table => {
     console.log('the tables', table);
   })
