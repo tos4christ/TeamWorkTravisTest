315db7185494b13b4fb09f80337bc0e00f6292ce
Tos4christ <glmmission@hotmail.com>
2019-11-07 19:24:49 +0100

    update queries callback for database

--- Models/poolConnection.js
+++ Models/poolConnection.js
@@ -6,7 +6,7 @@ const pool = new Pool();
 pool.on('connect', () => console.log('connected to the database'));
 pool.query(query.employees_query, (err, res) => {
   if(err) console.error(err);
-  console.log(res);
+  console.log('employee table created');
 });
 pool.query(`insert into employees (firstname, lastname, email, employee_password, gender, jobrole, employee_no, department, creation_date) values ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *`,
 ['tosin', 'fetuga', 'gmail', 'tosinn', 'male', 'IT', 'FS', Date().split('GMT')[0]])
