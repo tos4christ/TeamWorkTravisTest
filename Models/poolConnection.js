13fc34abac6d0a862b66d29eb8e697ee05bb89ac
Tos4christ <glmmission@hotmail.com>
2019-11-07 12:07:32 +0100

    ft-upload-files-to-test

--- Models/poolConnection.js
+++ Models/poolConnection.js
@@ -11,4 +11,32 @@ const pool = new Pool({
   max: 50
 })
 
+pool.query(`
+  CREATE TABLE public.employees
+  (
+    employee_id integer NOT NULL DEFAULT nextval('employees_employee_id_seq'::regclass),
+    firstname text NOT NULL,
+    lastname text NOT NULL,
+    email text NOT NULL,
+    employee_password text NOT NULL,
+    gender text NOT NULL,
+    jobrole text NOT NULL,
+    employee_no integer NOT NULL,
+    department text NOT NULL,
+    creation_date date NOT NULL,
+    CONSTRAINT employees_pkey PRIMARY KEY (employee_id),
+    CONSTRAINT employees_email_key UNIQUE (email),
+    CONSTRAINT employees_employee_no_key UNIQUE (employee_no)
+  )
+  WITH (
+    OIDS=FALSE
+  );
+  ALTER TABLE public.employees
+    OWNER TO postgres;
+  `)
+  .then( table => {
+    console.log('the tables', table);
+  })
+  .catch( e => console.error(e));
+
 module.exports = pool;
