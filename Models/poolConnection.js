695f61d6d6be3486ee9c8aaded062b6d0d405816
Tos4christ <glmmission@hotmail.com>
2019-11-07 19:04:52 +0100

    update queries callback for database

--- Models/poolConnection.js
+++ Models/poolConnection.js
@@ -1,7 +1,11 @@
 const { Pool } = require('pg');
+const query = require('./poolQuery');
 
 const pool = new Pool();
 
 pool.on('connect', () => console.log('connected to the database'));
+pool.query(query.employees_query, (err, res) => {
+  console.log(res);
+});
 
 module.exports = pool;
