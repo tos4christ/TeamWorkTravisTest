2ef574ccca59a337a3fcf4f66f02686a1e640d28
Tos4christ <glmmission@hotmail.com>
2019-11-07 18:48:20 +0100

    update queries callback for database

--- db.js
+++ db.js
@@ -13,44 +13,9 @@ const pool = new Pool({
 });
 
 pool.query(query.gif_table_query)
-  .then(() => {
-    console.log('gif table created');
-    pool.query(query.gif_comment_query)
-      .then(() => {
-        console.log('gif comment created');
-        pool.query(query.employees_query)
-          .then(() => {
-            console.log('employees table created');
-            pool.query(query.comments_table_query)
-              .then(() => {
-                console.log('comments table created');
-                pool.query(query.articles_query)
-                  .then(() => {
-                    console.log('articles table created');
-                    pool.query(query.article_comment_query)
-                      .then(() => {
-                        console.log('article comments table created');
-                        pool.query(query.admin_table_query)
-                          .then(() => {
-                            console.log('admin table created');
-                            pool.query(query.insertValue)
-                              .then(() => {
-                                console.log('values inserted');
-                              })
-                              .catch(e => console.error(e));
-                          })
-                          .catch( e => console.error(e));
-                      })
-                      .catch(e => console.error(e));
-                  })
-                  .catch( e => console.error(e));
-              })
-              .catch( e => console.error(e));
-          })
-          .catch( e => console.error(e));
-      })
-      .catch( e => console.error(e));
-  })  
+  .then((table) => {
+    console.log('gif table created', table);
+  })
   .catch( e => console.error(e));
 
 module.exports = pool;
