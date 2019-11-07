45d53d07056778d81ee2c07ba998f4276afc568d
Tos4christ <glmmission@hotmail.com>
2019-11-07 22:33:51 +0100

    update queries callback for database

--- Controllers/articleController.js
+++ Controllers/articleController.js
@@ -1,8 +1,10 @@
-const pool = require('../Models/poolConnection');
+//const pool = require('../Models/poolConnection');
+const { Pool } = require('pg').Pool;
 const userDetails = require('../utilities/getTokenUser');
 const articleSchema = require('../Models/articleSchema');
 
 const articleController = {};
+const pool = new Pool();
 
 articleController.createArticle = (req, res, next) => {
   const token = req.headers.authorization.split(' ')[1] ? req.headers.authorization.split(' ')[1] : req.headers.authorization;
