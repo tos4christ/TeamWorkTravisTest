beeeaec4d912c27de244196420254886c78681b4
Tos4christ <glmmission@hotmail.com>
2019-11-08 16:52:10 +0100

    errconnrefused bug solution

--- spec/spec.js
+++ spec/spec.js
@@ -65,7 +65,7 @@ describe('JWT route protection', () => {
           'authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjkwMjMwNywidXNlcm5hbWUiOiJnbnp0cmFkZUBnbWFpbC5jb20iLCJpYXQiOjE1NzMyMjQ2NDEsImV4cCI6MTU3NDY2NDY0MX0.XGlcBEz7rukL9KbrxI2HEcbVSVneFNUD2LTGD09e6Zw'
         },
         url: 'http://localhost:3000/api/v1/articles',
-        form: {article_id: 10000, title: 'test article', article: 'this is a test article', appr_status: true}
+        form: {article_id: 10002, title: 'test article', article: 'this is a test article', appr_status: true}
         }, (err, res, body) => {
         if(err) console.error(err);
         data.status = res.statusCode;
