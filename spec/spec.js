c810513f799e03fce3d16bf22ec5bee1b4ae1eca
Tos4christ <glmmission@hotmail.com>
2019-11-11 13:39:05 +0100

    all is ready now without babel yet

--- spec/spec.js
+++ spec/spec.js
@@ -289,7 +289,7 @@ describe('API endpoint tests', () => {
           'content-type': 'application/x-www-form-urlencoded',
           'authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjkwMjMwNywidXNlcm5hbWUiOiJnbnp0cmFkZUBnbWFpbC5jb20iLCJpYXQiOjE1NzMyMjQ2NDEsImV4cCI6MTU3NDY2NDY0MX0.XGlcBEz7rukL9KbrxI2HEcbVSVneFNUD2LTGD09e6Zw'
         },
-        url: 'http://localhost:3000/api/v1/gifs/7/comment',
+        url: 'http://localhost:3000/api/v1/gifs/9/comment',
         form: {
           "comments": "This is a new gif comment"
         }
@@ -371,7 +371,7 @@ describe('API endpoint tests', () => {
           'content-type': 'application/x-www-form-urlencoded',
           'authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjkwMjMwNywidXNlcm5hbWUiOiJnbnp0cmFkZUBnbWFpbC5jb20iLCJpYXQiOjE1NzMyMjQ2NDEsImV4cCI6MTU3NDY2NDY0MX0.XGlcBEz7rukL9KbrxI2HEcbVSVneFNUD2LTGD09e6Zw'
         },
-        url: 'http://localhost:3000/api/v1/gifs/7'
+        url: 'http://localhost:3000/api/v1/gifs/10'
       }, (err, resp, body) => {
         if(err) throw err;
         data.status = resp.statusCode;
