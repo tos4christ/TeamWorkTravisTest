74ade196a33832d00f47e946b6afda448e97dacf
Tos4christ <glmmission@hotmail.com>
2019-11-12 10:46:23 +0100

    updated database data for the gif

--- app/spec/spec.js
+++ app/spec/spec.js
@@ -367,7 +367,7 @@ describe('API endpoint tests', () => {
           'content-type': 'application/x-www-form-urlencoded',
           'authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjkwMjMwNywidXNlcm5hbWUiOiJnbnp0cmFkZUBnbWFpbC5jb20iLCJpYXQiOjE1NzMyMjQ2NDEsImV4cCI6MTU3NDY2NDY0MX0.XGlcBEz7rukL9KbrxI2HEcbVSVneFNUD2LTGD09e6Zw'
         },
-        url: 'http://localhost:3000/api/v1/gifs/38'
+        url: 'http://localhost:3000/api/v1/gifs/19'
       }, (err, resp, body) => {
         if(err) throw err;
         data.status = resp.statusCode;
